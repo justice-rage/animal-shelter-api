bed3fec82e86d0d3be4648f0bd72ba386af691d7
justicepelteir <justice.douglas@hey.com>
2022-06-30 12:46:50 -0700

    create proxy shelter animal info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+    def self.begin
+      seed = Seed.new
+      seed.generate_adoptable_animals
+    end
+  
+    def generate_adoptable_animals
+      20.times do |i|
+        type = ['cat', 'dog'].sample
+        species_breed = type == 'cat' ? Faker::Creature::Cat.breed : Faker::Creature::Dog.breed
+        animal_age = ['baby', 'adolescent', 'adult', 'senior'].sample
+        adoptable_animal = AdoptableAnimal.create!(
+            species: type,
+            breed: species_breed,
+            name: Faker::Name.first_name,
+            age: animal_age,
+            sex: Faker::Gender.binary_type
+        )
+        puts "Adoptable Animal: #{i},
+        Species: #{adoptable_animal.species}, 
+        Breed: #{adoptable_animal.breed}, 
+        Name: #{adoptable_animal.name}, 
+        Age: #{adoptable_animal.age}, 
+        Sex: #{adoptable_animal.sex}"
+      end
+    end
+  end
+  
+  Seed.begin
