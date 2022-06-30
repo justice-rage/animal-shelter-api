edbc792e3e192182c5b703c77adee17cf915eb8b
justicepelteir <justice.douglas@hey.com>
2022-06-30 13:04:23 -0700

    add show, create, update, destroy routes

--- app/controllers/adoptable_animals_controller.rb
+++ app/controllers/adoptable_animals_controller.rb
@@ -1,12 +1,36 @@
 class AdoptableAnimalsController < ApplicationController
 
     def index
-      @adoptable_animals = {"adoptable animal": "species: cat, name: velma, breed: unknown, color: tuxedo"}
+      @adoptable_animals = AdoptableAnimal.all
       json_response(@adoptable_animals)
+    end
+
+    def show
+      @adoptable_animal = AdoptableAnimal.find(params[:id])
+      json_response(@adoptable_animal)
+    end
+
+    def create
+      @adoptable_animal = AdoptableAnimal.create(adoptable_animal_params)
+      json_response(@adoptable_animal)
+    end
+
+    def update
+      @adoptable_animal = AdoptableAnimal.find(params[:id])
+      @adoptable_animal.update(adoptable_animal_params)
+    end
+
+    def destroy
+      @adoptable_animal = AdoptableAnimal.find(params[:id])
+      @adoptable_animal.destroy
     end
   
     private
     def json_response(object, status = :ok)
       render json: object, status: status
+    end
+
+    def adoptable_animal_params
+      params.permit(:species, :breed, :name, :age, :sex)
     end
   end
