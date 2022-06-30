dcef8d5101baa4a4abf6a0b2f67448768eaa6b9c
justicepelteir <justice.douglas@hey.com>
2022-06-30 15:46:32 -0700

    non-passing test: expected 20, got 24 ---> (compared using ==)

--- spec/requests/get_adoptable_animals_spec.rb
+++ spec/requests/get_adoptable_animals_spec.rb
@@ -5,6 +5,10 @@ describe "get all adoptable animals route", :type => :request do
 
   before { get '/adoptable_animals'}
 
+  it 'returns all adoptable animals' do
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+
   it 'returns status code 200' do
     expect(response).to have_http_status(:success)
   end
