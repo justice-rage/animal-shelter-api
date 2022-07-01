91e25111bca5ee3c989366c7e96ea60a72ab1df6
justicepelteir <justice.douglas@hey.com>
2022-06-30 17:40:44 -0700

    Tests passing

---
+++ spec/requests/post_route_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "post a adoptable animal route", :type => :request do
+
+  before do
+    post '/adoptable_animals', params: { :species => 'cat', :breed => 'tabby', :name => 'leo', :age => '1', :sex => 'male' }
+  end
+
+  it 'returns the adoptable animal name' do
+    expect(JSON.parse(response.body)['species']).to eq('cat')
+  end
+
+  it 'returns the adoptable animal breed' do
+    expect(JSON.parse(response.body)['breed']).to eq('tabby')
+  end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
+end
