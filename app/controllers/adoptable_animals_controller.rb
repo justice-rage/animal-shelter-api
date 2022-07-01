93817823e9cc398fa53bfd9ac2d2743d4a45e64c
justicepelteir <justice.douglas@hey.com>
2022-06-30 17:40:09 -0700

    Fix Bug: CRUD functionality restored

--- app/controllers/adoptable_animals_controller.rb
+++ app/controllers/adoptable_animals_controller.rb
@@ -1,41 +1,41 @@
 class AdoptableAnimalsController < ApplicationController
 
-    def index
-      @adoptable_animals = AdoptableAnimal.all
-      json_response(@adoptable_animals)
-    end
+  def index
+    @adoptable_animals = AdoptableAnimal.all
+    json_response(@adoptable_animals)
+  end
 
-    def show
-      @adoptable_animal = AdoptableAnimal.find(params[:id])
-      json_response(@adoptable_animal)
-    end
+  def show
+    @adoptable_animal = AdoptableAnimal.find(params[:id])
+    json_response(@adoptable_animal)
+  end
 
-    def create
-      @adoptable_animal = AdoptableAnimal.create!(adoptable_animal_params)
-      json_response(@adoptable_animal)
-    end
+  def create
+    @adoptable_animal = AdoptableAnimal.create!(adoptable_animal_params)
+    json_response(@adoptable_animal, :created)
+  end
 
-    def update
-      @adoptable_animal = AdoptableAnimal.find(params[:id])
-      if @adoptable_animal.update!(adoptable_animal_params)
-        render status: 200, json: {
-          message: "Update successful."
-        }
-      end
+  def update
+    @adoptable_animal = AdoptableAnimal.find(params[:id])
+    if @adoptable_animal.update!(adoptable_animal_params)
+      render status: 200, json: {
+        message: "Update successful."
+      }
     end
+  end
 
-    def destroy
-      @adoptable_animal = AdoptableAnimal.find(params[:id])
-      if @adoptable_animal.destroy
-        render status: 200, json: {
-          message: "Destruction successful."
-        }
+  def destroy
+    @adoptable_animal = AdoptableAnimal.find(params[:id])
+    if @adoptable_animal.destroy
+      render status: 200, json: {
+        message: "Destruction successful."
+      }
     end
-  
-    private
+  end
 
-    def adoptable_animal_params
-      params.permit(:species, :breed, :name, :age, :sex)
-    end
+  private
+
+  def adoptable_animal_params
+    params.permit(:species, :breed, :name, :age, :sex)
   end
 end
