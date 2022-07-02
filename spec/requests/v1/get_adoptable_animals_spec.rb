b3ba3ed04a131c5d0eb97ae6c872ec54c762015f
justicepelteir <justice.douglas@hey.com>
2022-07-02 16:30:19 -0700

    tests passing

--- spec/requests/v1/get_adoptable_animals_spec.rb
+++ spec/requests/v1/get_adoptable_animals_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 describe "get all adoptable animals route", :type => :request do
-  let!(:adoptable_animals) { FactoryBot.create_list(:adoptable_animal, 20)}
+  let!(:adoptable_animals) { FactoryBot.create_list(:adoptable_animal, 100)}
 
   before { get '/v1/adoptable_animals'}
 
   it 'returns all adoptable animals' do
     # Passing: rake db:reset fixed failing test
     #             - unsure how
-    expect(JSON.parse(response.body).size).to eq(20)
+    expect(JSON.parse(response.body).size).to eq(100)
+  end
+
+  it 'returns 25 beasts at a time' do
+    get '/v1/adoptable_animals?page=1'
+    expect(JSON.parse(response.body).size).to eq(25)
   end
 
   it 'returns status code 200' do
