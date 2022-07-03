2d1f8a26a6c7b4f16982338e342ae9ed293c0eee
justicepelteir <justice.douglas@hey.com>
2022-07-02 18:44:18 -0700

    scope search tests passing

--- spec/requests/v1/get_adoptable_animals_spec.rb
+++ spec/requests/v1/get_adoptable_animals_spec.rb
@@ -19,4 +19,37 @@ describe "get all adoptable animals route", :type => :request do
   it 'returns status code 200' do
     expect(response).to have_http_status(:success)
   end
+
+  describe 'get animal by search', :type => :request do
+
+    before do
+      post '/v1/adoptable_animals', params: { :species => 'cat', :breed => 'tuxedo', :name => "velma", :age => 'adolescent', :sex => "female"}
+    end
+      
+    it 'returns an adoptable animal by species' do
+      get '/v1/adoptable_animals?species=cat'
+      expect(JSON.parse(response.body)[0]['species']).to eq('cat')
+    end
+
+    it 'returns an adoptable animal by breed' do
+      get '/v1/adoptable_animals?breed=tuxedo'
+      expect(JSON.parse(response.body)[0]['breed']).to eq('tuxedo')
+    end
+
+    it 'returns an adoptable animal by name' do
+      get '/v1/adoptable_animals?name=velma'
+      expect(JSON.parse(response.body)[0]['name']).to eq('velma')
+    end
+
+    it 'returns an adoptable animal by age' do
+      get '/v1/adoptable_animals?age=adolescent'
+      expect(JSON.parse(response.body)[0]['age']).to eq('adolescent')
+    end
+  
+    it 'returns an animal by sex' do
+      get '/v1/adoptable_animals?sex=female'
+      expect(JSON.parse(response.body)[0]['sex']).to eq('female')
+    end
+  end
+  
 end
