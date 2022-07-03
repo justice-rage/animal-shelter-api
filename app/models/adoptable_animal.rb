88952f3518880bf06f063fab0e04c86c139072b6
justicepelteir <justice.douglas@hey.com>
2022-07-02 18:30:27 -0700

    add scope based search methods

--- app/models/adoptable_animal.rb
+++ app/models/adoptable_animal.rb
@@ -1,2 +1,9 @@
 class AdoptableAnimal < ApplicationRecord
+
+    scope :search_species, -> (species_parameter) { where("species ilike ?", "%#{species_parameter}%")}
+    scope :search_breed, -> (breed_parameter) { where("breed ilike ?", "%#{breed_parameter}%")}
+    scope :search_name, -> (name_parameter) { where("name ilike ?", "%#{name_parameter}%")}
+    scope :search_age, -> (age_parameter) { where("age ilike ?", "%#{age_parameter}%")}
+    scope :search_sex, -> (sex_parameter) { where("sex ilike ?", "%#{sex_parameter}%")}
+
 end
