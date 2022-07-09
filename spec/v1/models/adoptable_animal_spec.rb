583f7cccd4ba93fac1a800b93372b42e4521ee6f
justicepelteir <justice.douglas@hey.com>
2022-07-08 18:41:46 -0700

    make validation tests passing

--- spec/v1/models/adoptable_animal_spec.rb
+++ spec/v1/models/adoptable_animal_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-  describe AdoptableAnimal do
-    it { should validate_presence_of :species }
-    it { should validate_presence_of :breed }
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :age }
-    it { should validate_presence_of :sex }
+describe AdoptableAnimal, type: :model do
+  it { should validate_presence_of :species }
+  it { should validate_presence_of :breed }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :sex }
 end
